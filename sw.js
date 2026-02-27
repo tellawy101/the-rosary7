c3918d60d9bf89612f7edae50cbad1e3cfa6d683
tellawy101 <166151435+tellawy101@users.noreply.github.com>
2026-02-27 09:55:34 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'zekr-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
